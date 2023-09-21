b2deaa4e058c9832910c2cedcea69f1a11476700
Abu Zaid <zaid70979@gmail.com>
2023-09-21 12:13:20 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+     <h1 className=" bg-primary-800 text-white">asdfdsaf</h1>
+     <h1 className=" bg-secondary-400 text-black">asdfdsaf</h1>
+    </>
+  )
+}
+
+export default App
